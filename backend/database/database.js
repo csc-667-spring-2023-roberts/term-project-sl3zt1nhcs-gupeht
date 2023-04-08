1b918e6152ef8d42f05d37466b6a2d3f92494915
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-04-08 06:18:05 -0700

    Error handling implemented on create tables

--- backend/database/database.js
+++ backend/database/database.js
@@ -14,6 +14,11 @@ to create the database run the code node database.js
 const { Client } = require("pg");
 const dotenv = require("dotenv").config({ path: "../.env" });
 
+if (dotenv.error) {
+  console.error("Error loading .env file:", dotenv.error);
+  process.exit(1);
+}
+
 const connectionString = {
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 };
@@ -25,25 +30,28 @@ const client = new Client(connectionString);
 const createTable = async () => {
   try {
     await client.connect();
+  } catch (e) {
+    console.error("Error connecting to database:", e);
+    process.exit(1);
+  }
 
-    await client.query(`
+  const tables = [
+    `
       CREATE TABLE IF NOT EXISTS public.users (
         user_id SERIAL PRIMARY KEY,
         username VARCHAR(50) NOT NULL UNIQUE,
         password VARCHAR(100) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         balance INTEGER NOT NULL DEFAULT 0
-      ); 
-    `);
-
-    await client.query(`
+      );
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.tables (
         table_id SERIAL PRIMARY KEY,
         table_name VARCHAR(50) NOT NULL UNIQUE
       );
-    `);
-
-    await client.query(`
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.players (
         player_id SERIAL PRIMARY KEY,
         user_id INTEGER NOT NULL REFERENCES users(user_id),
@@ -51,9 +59,8 @@ const createTable = async () => {
         balance INTEGER NOT NULL DEFAULT 0,
         status VARCHAR(20) NOT NULL DEFAULT 'active'
       );
-    `);
-
-    await client.query(`
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.games (
         game_id SERIAL PRIMARY KEY,
         table_id INTEGER NOT NULL REFERENCES tables(table_id),
@@ -61,17 +68,15 @@ const createTable = async () => {
         end_time TIMESTAMP,
         winner_id INTEGER REFERENCES players(player_id)
       );
-    `);
-
-    await client.query(`
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.cards (
         card_id SERIAL PRIMARY KEY,
         suit VARCHAR(10) NOT NULL,
         rank VARCHAR(5) NOT NULL
       );
-    `);
-
-    await client.query(`
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.hands (
         hand_id SERIAL PRIMARY KEY,
         player_id INTEGER NOT NULL REFERENCES players(player_id),
@@ -79,22 +84,36 @@ const createTable = async () => {
         game_id INTEGER NOT NULL REFERENCES games(game_id),
         is_community_hand BOOLEAN NOT NULL DEFAULT FALSE
       );
-    `);
-
-    await client.query(`
+    `,
+    `
       CREATE TABLE IF NOT EXISTS public.card_in_hand (
         card_id INTEGER NOT NULL REFERENCES cards(card_id),
         hand_id INTEGER NOT NULL REFERENCES hands(hand_id),
         PRIMARY KEY (card_id, hand_id)
       );
-    `);
+    `,
+  ];
 
-    console.log("Tables created. Check your local Db server.");
-  } catch (e) {
-    console.error("Error creating tables:", e);
-  } finally {
-    client.end();
-  }
+  const createNextTable = (index) => {
+    if (index >= tables.length) {
+      console.log("Tables created. Check your local Db server.");
+      return client.end();
+    }
+
+    return client
+      .query(tables[index])
+      .then(() => {
+        console.log(`Table ${index + 1} created.`);
+        return createNextTable(index + 1);
+      })
+      .catch((e) => {
+        console.error(`Error creating table ${index + 1}:`, e);
+        return client.end();
+      });
+  };
+
+  createNextTable(0);
 };
+
 
 createTable();
