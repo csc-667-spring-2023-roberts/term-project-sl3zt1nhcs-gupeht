c95a4f058ad81b28d1589970a585bef41cf68f02
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-04-08 06:30:36 -0700

    comments added

--- backend/database/database.js
+++ backend/database/database.js
@@ -11,23 +11,30 @@ DB_NAME=texas_poker
 to create the database run the code node database.js
 */
 
-const { Client } = require("pg");
+// import Client class
+const { Client } = require("pg"); 
+// the config path must be set to the root of the project directory
 const dotenv = require("dotenv").config({ path: "../.env" });
 
+// error check handler for dotenv
 if (dotenv.error) {
   console.error("Error loading .env file:", dotenv.error);
   process.exit(1);
 }
 
+// constains all the environment variables to connect to database
 const connectionString = {
   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 };
-
+// implement to help to debug an issues
 console.log(connectionString);
-
+//Create a  new instance of Client
 const client = new Client(connectionString);
 
+//  connects client object to your local db server and creates tables
+
 const createTable = async () => {
+  //  A try and catch block to connect the client to the datbase
   try {
     await client.connect();
   } catch (e) {
@@ -35,7 +42,10 @@ const createTable = async () => {
     process.exit(1);
   }
 
+  // Array of tables
   const tables = [
+
+    // Index 1
     `
       CREATE TABLE IF NOT EXISTS public.users (
         user_id SERIAL PRIMARY KEY,
@@ -45,12 +55,14 @@ const createTable = async () => {
         balance INTEGER NOT NULL DEFAULT 0
       );
     `,
+    // Index 2
     `
       CREATE TABLE IF NOT EXISTS public.tables (
         table_id SERIAL PRIMARY KEY,
         table_name VARCHAR(50) NOT NULL UNIQUE
       );
     `,
+    // Index 3
     `
       CREATE TABLE IF NOT EXISTS public.players (
         player_id SERIAL PRIMARY KEY,
@@ -60,6 +72,7 @@ const createTable = async () => {
         status VARCHAR(20) NOT NULL DEFAULT 'active'
       );
     `,
+    // Index 4
     `
       CREATE TABLE IF NOT EXISTS public.games (
         game_id SERIAL PRIMARY KEY,
@@ -69,6 +82,7 @@ const createTable = async () => {
         winner_id INTEGER REFERENCES players(player_id)
       );
     `,
+    //Index 5
     `
       CREATE TABLE IF NOT EXISTS public.cards (
         card_id SERIAL PRIMARY KEY,
@@ -76,6 +90,7 @@ const createTable = async () => {
         rank VARCHAR(5) NOT NULL
       );
     `,
+    // Index 6
     `
       CREATE TABLE IF NOT EXISTS public.hands (
         hand_id SERIAL PRIMARY KEY,
@@ -85,6 +100,7 @@ const createTable = async () => {
         is_community_hand BOOLEAN NOT NULL DEFAULT FALSE
       );
     `,
+    // Index 7
     `
       CREATE TABLE IF NOT EXISTS public.card_in_hand (
         card_id INTEGER NOT NULL REFERENCES cards(card_id),
@@ -94,12 +110,17 @@ const createTable = async () => {
     `,
   ];
 
+  // function that creates tables recursively according to position. Order matter due to the relations
   const createNextTable = (index) => {
+
+    // Base Case: If all tables have been creaed will exit
     if (index >= tables.length) {
       console.log("Tables created. Check your local Db server.");
       return client.end();
     }
 
+    // Recursion here : promises are used to handle eror
+    //  index refer to the position of table in tables
     return client
       .query(tables[index])
       .then(() => {
@@ -112,6 +133,7 @@ const createTable = async () => {
       });
   };
 
+  // function is called inside starting at array 0
   createNextTable(0);
 };
 
