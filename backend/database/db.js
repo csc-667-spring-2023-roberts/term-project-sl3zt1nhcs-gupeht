9e258aaf54e408cfe216d581fff71d91d42f45fa
Mohamed Sharif <mohamedsharif@Mohameds-MacBook-Pro.local>
2023-04-16 05:09:03 -0700

    db.js create a database using promises to match the other apis that uses promises

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,6 +3,19 @@ const config = require(` ./config/${process.env.NODE_ENW || 'development'}`);
 
 const pool = new Pool(config.database);
 
-module.exports = {
-    query: (text, params) => pool.query(text,params),
-}
+const db = {
+    query: (text,params) =>{
+        return new Promise( (resolve,reject) =>{
+            pool.query(text,params,(err,res)=>{
+                if (err){
+                    reject(err)
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        });
+    },
+};
+
+module.exports = db;
