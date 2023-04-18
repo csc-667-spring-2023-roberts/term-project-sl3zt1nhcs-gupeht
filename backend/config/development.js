97aa3debe4add49ef9fbcc448aabf5d1a6875dfb
Mohamed Sharif <mohamedsharif@sfs-wifi-dhcp-10-143-163-28.wifi.sfsu.edu>
2023-04-18 10:44:54 -0700

    added  database env_variables to connect. Created a function to test connection. Connection was successful. Put your database details there

--- backend/config/development.js
+++ backend/config/development.js
@@ -1,3 +1,11 @@
 module.exports ={
+    database:{
+        host:"localhost",
+        port: "5432" ,
+        user:"mohamedsharif",
+        password:"", // default
+       database:"postgres",
+    },
+
     PORT: 3000,
 };
--- backend/database/db.js
+++ backend/database/db.js
@@ -17,4 +17,26 @@ const db = {
     },
 };
 
+async function test_connection(){
+
+    try{    
+        pool.query('SELECT current_database();',(err, res)=>{
+            if (err){
+                console.error("connection to the database failed",err);
+            }
+            else{
+                console.log("Connection to the database was successful",res.rows[0].current_database);
+            }
+        })
+    }catch(err){
+
+        if (err){
+            console.error("there was a error connecting to the database", err);
+        }
+}
+
+}
+
+test_connection();
+
 module.exports = db;
