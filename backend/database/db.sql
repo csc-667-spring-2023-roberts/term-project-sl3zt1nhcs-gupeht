315d4e708e7e0688c7c480404ca297c9a24527e1
Mohamed Sharif <mohamedsharif@sfs-wifi-dhcp-10-143-163-28.wifi.sfsu.edu>
2023-04-18 09:57:22 -0700

    made final fixes on the database by adding CREATE TABLE IF NOT EXIST. Also, I created table localy and works without errors

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE users (
 CREATE INDEX idx_users_username ON users(username);
 CREATE INDEX idx_users_email ON users(email);
 
-CREATE TABLE players (
+CREATE TABLE IF NOT EXISTS players (
   player_id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
   game_id INTEGER NOT NULL,
@@ -19,14 +19,14 @@ CREATE INDEX idx_players_user_id ON players(user_id);
 CREATE INDEX idx_players_game_id ON players(game_id);
 CREATE INDEX idx_players_status ON players(status);
 
-CREATE TABLE game_tables (
+CREATE TABLE IF NOT EXISTS game_tables (
   table_id SERIAL PRIMARY KEY,
   max_players INTEGER NOT NULL DEFAULT 9,
   dealer_id INTEGER REFERENCES players(player_id),
   small_blind INTEGER NOT NULL DEFAULT 1
 );
 
-CREATE TABLE lobbies (
+CREATE TABLE  IF NOT EXISTS lobbies (
   lobby_id SERIAL PRIMARY KEY,
   lobby_owner INTEGER NOT NULL REFERENCES users(user_id),
   num_players INTEGER NOT NULL,
@@ -34,9 +34,9 @@ CREATE TABLE lobbies (
   starting_chips INTEGER NOT NULL
 );
 
-CREATE TABLE games (
+CREATE TABLE  IF NOT EXISTS games (
   game_id INTEGER PRIMARY KEY REFERENCES lobbies(lobby_id) ON DELETE CASCADE,
-  table_id INTEGER NOT NULL REFERENCES game_tables(table_id),
+  table_id INTEGER NOT NULL REFERENCES game_tables(table_id) ON DELETE CASCADE,
   start_time TIMESTAMP NOT NULL,
   end_time TIMESTAMP,
   winner_id INTEGER REFERENCES players(player_id),
@@ -49,7 +49,7 @@ CREATE TABLE games (
 CREATE INDEX idx_games_table_id ON games(table_id);
 CREATE INDEX idx_games_winner_id ON games(winner_id);
 
-CREATE TABLE pots (
+CREATE TABLE  IF NOT EXISTS pots (
   pot_id SERIAL PRIMARY KEY,
   game_id INTEGER NOT NULL REFERENCES games(game_id) ON DELETE CASCADE,
   pot_amount INTEGER NOT NULL DEFAULT 0,
@@ -58,7 +58,7 @@ CREATE TABLE pots (
 
 CREATE INDEX idx_pots_game_id ON pots(game_id);
 
-CREATE TABLE side_pots (
+CREATE TABLE IF NOT EXISTS side_pots (
   pot_id INTEGER NOT NULL REFERENCES pots(pot_id) ON DELETE CASCADE,
   side_pot_number INTEGER NOT NULL,
   side_pot_amount INTEGER NOT NULL DEFAULT 0,
@@ -68,7 +68,7 @@ CREATE TABLE side_pots (
 CREATE INDEX idx_side_pots_pot_id ON side_pots(pot_id);
 
 
-CREATE TABLE lobby_players (
+CREATE TABLE  IF NOT EXISTS lobby_players (
   lobby_id INTEGER NOT NULL REFERENCES lobbies(lobby_id) ON DELETE CASCADE,
   player_id INTEGER NOT NULL REFERENCES players(player_id) ON DELETE CASCADE,
   PRIMARY KEY (lobby_id, player_id)
@@ -79,7 +79,7 @@ CREATE INDEX idx_lobby_players_player_id ON lobby_players(player_id);
 
 -- The rest of the tables are not changed
 
-CREATE TABLE cards (
+CREATE TABLE IF NOT EXISTS  cards (
   card_id SERIAL PRIMARY KEY,
   suit VARCHAR(10) NOT NULL,
   rank VARCHAR(5) NOT NULL
@@ -87,7 +87,7 @@ CREATE TABLE cards (
 
 CREATE INDEX idx_cards_suit_rank ON cards(suit, rank);
 
-CREATE TABLE hands (
+CREATE TABLE  IF NOT EXISTS hands (
   hand_id SERIAL PRIMARY KEY,
   player_id INTEGER NOT NULL REFERENCES players(player_id) ON DELETE CASCADE,
   table_id INTEGER NOT NULL REFERENCES game_tables(table_id) ON DELETE CASCADE, 
@@ -99,7 +99,7 @@ CREATE INDEX idx_hands_player_id ON hands(player_id);
 CREATE INDEX idx_hands_table_id ON hands(table_id);
 CREATE INDEX idx_hands_game_id ON hands(game_id);
 
-CREATE TABLE card_in_hand (
+CREATE TABLE  IF NOT EXISTS card_in_hand (
   card_id INTEGER NOT NULL REFERENCES cards(card_id),
   hand_id INTEGER NOT NULL REFERENCES hands(hand_id) ON DELETE CASCADE,
   PRIMARY KEY (card_id, hand_id)
