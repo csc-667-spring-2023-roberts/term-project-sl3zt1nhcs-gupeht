26ad208b12950c9a34df01f3f2a828df9de51bf8
Mohamed Sharif <mohamedsharif@Mohameds-MacBook-Pro.local>
2023-04-20 01:42:49 -0700

    implementing a object called result on controllers. the object will be sent out to front end. Currently on table controller. Need to add to all controllers.

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -4,25 +4,53 @@ const {CustomError}= require('../middleware/customErrorHandler');
 exports.getPlayersAtTable= (req,res)=>{
 
     const {table_id} = req.params;
+    const result = {};
 
-    tableModel.getPlayersAtTable(table_id).then((result)=>{
-        res.status(200).json({players:result});
+    tableModel.getPlayersAtTable(table_id).then((data)=>{
+
+        if ( data.length === 0){
+            result.message = " No players found at table"
+        }
+        else{
+            result.players = data;
+            res.status(200).json({Result: result});
+        }
     })
-    .catch(err=>{
-        res.status(500).json({error:err.message});
-    });
+    .catch((err=>{
+        result.error="Internal server error";
+        res.status(500).json({Result:result});
+    }));
 
 };
 
 exports.addPlayerToTable = (req,res)=>{
 
     const {table_id, player_id} = req.params;
+    const result = {};
 
-    tableModel.addPlayerToTable(table_id,player_id).then((result)=>{
-        res.status(200).json({table:table_id,player:player_id});
+    tableModel.addPlayerToTable(table_id,player_id).then((data)=>{
+
+        if ( data.affectedRows === 0){
+            result.error = "Could not add player to table";
+            res.status(400).json({Result:result});
+        }
+        else{
+            result.message = "Player added to table successfully";
+            result.table = table_id;
+            result.player = player_id;
+            res.status(200).json({Result:result});
+        }
     })
     .catch((err)=>{
-        res.status(500).json({error:err.message});
+        if (err.code == "ER_DUP_ENTRY"){
+            result.error = "Player already added to table";
+            res.status(400).json({Result:result});
+        }
+        else{
+            result.error = "Internal server error";
+            res.status(500).json({Result:result});
+        }
+       
     });
 
-}
+};
