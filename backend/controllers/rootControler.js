571ecefa8c07947d24e073bb1995e0be6d911fe5
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-04-24 16:59:23 -0700

    renamed project folder

--- backend/controllers/rootControler.js
+++
@@ -1,7 +0,0 @@
-
-exports.getHome = (req,res)=>{
-    res.render("Home page",{
-        title:"Texas Hold Game",
-        message:"Web app under development"
-    })
-}
--- backend/routes/root.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const rootController = require('../controllers/rootControler');
-
-router.get('/home',rootController.getHome);
-
-module.exports = router
