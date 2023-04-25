fe9dd396ff0c8e3e926a48ad59b1e4b429e589a5
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-04-25 15:46:41 -0700

    fixed dir for app.set(views)

--- texasHold-web-app/backend/src/server.js
+++ texasHold-web-app/backend/src/server.js
@@ -13,7 +13,7 @@ const setupSocket = require('./socket');
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname,'../../frontend/views'));
 app.set('view engine', 'ejs');
 
 // Serve static files
