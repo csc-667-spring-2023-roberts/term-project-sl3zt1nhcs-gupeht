53f1fbe29ce42b40cf8efa4ec9de212515962d6e
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-04-27 21:25:48 -0700

    fixed create user

--- texasHold-web-app/backend/src/controllers/userController.js
+++ texasHold-web-app/backend/src/controllers/userController.js
@@ -6,8 +6,10 @@ userController.createUser = (req, res) => {
   const { username, password, email } = req.body;
 
   userModel.createUser(username, password, email)
-    .then(() => {
-      res.status(200).json({ message: 'User created successfully' });
+    .then((user) => {
+      const token = user.auth_token;
+      delete user.auth_token;
+      res.status(201).json({ message: 'User created successfully', user, token });
     })
     .catch((err) => {
       res.status(err.status || 500).json({ message: err.message });
@@ -34,16 +36,9 @@ userController.getUserById = (req, res) => {
 userController.login = (req, res, next) => {
   const { username, password } = req.body;
 
-  userModel.login( username, password)
+  userModel.login(username, password)
     .then(async (user) => {
-      /*
-      In the userController.login method, after successfully
-      logging in, we need to set the user in the session
-      data so that we can retrieve it later. We can do
-      this by adding the following line of code:
-      */
       req.session.userId = user.user_id;
-      console.log(req.session.userId);
       const token = user.auth_token;
       delete user.auth_token;
 
@@ -54,11 +49,9 @@ userController.login = (req, res, next) => {
     });
 };
 
-
 userController.logout = async (req, res, next) => {
   if (req.method === 'POST' || req.method === 'GET') {
     try {
-      // Clear the JWT token from the database
       await userModel.clearAuthToken(req.user.sub);
 
       userModel.logout(req);
@@ -82,4 +75,5 @@ userController.getCurrentUser = (req, res, next) => {
       next();
     });
 };
+
 module.exports = userController;
