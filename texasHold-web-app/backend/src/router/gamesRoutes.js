50129442bdaf1f8472df33667fb2c00f1c4079d6
Mohamed Sharif <msharif3@mail.sfsu.edu>
2023-05-02 03:17:51 -0700

    back and fron end sockets seems to be connected. Needs lot work on main.js for front end and remode the game model controllers, routers...

--- texasHold-web-app/backend/src/router/gamesRoutes.js
+++ texasHold-web-app/backend/src/router/gamesRoutes.js
@@ -3,32 +3,12 @@ const router = express.Router();
 const gameController = require('../controllers/gameController');
 const authenticate  = require('../middleware/auth');
 
+
+
+
 // Create a new game
-router.post('/game', authenticate, gameController.createGame);
+router.post('/create', authenticate, gameController.createGame);
 
-// Load a game
-router.get('/game/:gameId', authenticate, gameController.getGameState);
-
-// Update a game
-router.put('/game/:gameId', authenticate, gameController.updateGame);
-
-// Add players to a game
-router.post('/game/:gameId/players', authenticate, gameController.addPlayersToGame);
-
-// Remove a player from a game
-router.delete('/game/:gameId/players/:playerId', authenticate, gameController.removePlayerFromGame);
-
-// Get table details
-router.get('/table/:tableId', authenticate, gameController.getTable);
-
-// Get the game state by game ID
-router.get('/game/:gameId/state', authenticate, gameController.getGameState);
-
-// Handle a player's action during a betting round
-router.post('/game/:gameId/player-action', authenticate, gameController.handlePlayerAction);
-
-// Play a round in a game
-router.put('/game/:gameId/play-round', authenticate, gameController.playRound);
 
 
 module.exports = router;
--- texasHold-web-app/frontend/src/public/js/main.js
+++ texasHold-web-app/frontend/src/public/js/main.js
@@ -1,78 +1,175 @@
 // public/js/main.js
 
-import { register } from './register';
-import { login } from './login';
-import { logout } from './logout';
+import { register } from "./register";
+import { login } from "./login";
+import { logout } from "./logout";
+import connectSocket from "./socket";
 
 export async function fetchLobby() {
-  const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
-  if (!token) {
-    // If the token is not found, redirect to the login page
-    location.href = '/user/login';
-    return;
-  }
-
-  
-  try {
-    const response = await fetch('/user/lobby', {
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    });
-
-    const lobbyHtml = await response.text();
-   
-
-    if (response.status === 200) {
-      // Render the lobby HTML
-      document.querySelector('body').innerHTML = lobbyHtml;
-    } else {
-      console.error('Error fetching lobby:', lobbyHtml);
+    if (!token) {
+        // If the token is not found, redirect to the login page
+        location.href = "/user/login";
+        return;
     }
-  } catch (error) {
-    console.error('Error fetching lobby:', error);
-  }
+
+    try {
+        const response = await fetch("/user/lobby", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        const lobbyHtml = await response.text();
+
+        if (response.status === 200) {
+            // Render the lobby HTML
+            document.querySelector("body").innerHTML = lobbyHtml;
+            // Fetch the game list when the lobby is loaded
+            getGameList();
+            // Connect the socket and join the game
+            const socket = connectSocket();
+
+            //TODO: const gameId = // Set the gameId from the game you want the user to join
+
+            socket.emit("join_game", gameId);
+        } else {
+            console.error("Error fetching lobby:", lobbyHtml);
+        }
+    } catch (error) {
+        console.error("Error fetching lobby:", error);
+    }
 }
 
-function checkLoggedIn(){
-  const token = localStorage.getItem('token');
-  if(token){
-    fetchLobby();
-  }
+// Fetch the game list when the lobby is loaded
+if (window.location.pathname === "/user/lobby") {
+  getGameList();
 }
 
+// prevents from going to login page
+function checkLoggedIn() {
+    const token = localStorage.getItem("token");
+    if (token) {
+        fetchLobby();
+    }
+}
+
+// Event listener  for register and login forms
 document.addEventListener("DOMContentLoaded", () => {
-  const registerForm = document.getElementById("register-form");
-  const loginForm = document.getElementById("login-form");
-  const logoutButton = document.getElementById("logout");
+    const registerForm = document.getElementById("register-form");
+    const loginForm = document.getElementById("login-form");
 
+    if (registerForm) {
+        registerForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            register(event);
+        });
+    }
 
+    if (loginForm) {
+        loginForm.addEventListener("submit", (event) => {
+            event.preventDefault();
+            login(event);
+        });
+    }
 
-  if (registerForm) {
-    registerForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      register(event);
-    });
-  }
-
-  if (loginForm) {
-    loginForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      login(event);
-    });
-  }
-
-  // stop from going to log in if token exist
-  checkLoggedIn();
-
+    // stop from going to log in if token exist
+    checkLoggedIn();
 });
 
 
- // Add a separate event listener for the logout button
- document.addEventListener('click', (event) => {
-  if (event.target && event.target.id === 'logout') {
-    event.preventDefault();
-    logout();
-  }
+
+// Add a separate event listener for the logout button
+document.addEventListener("click", (event) => {
+    if (event.target && event.target.id === "logout") {
+        event.preventDefault();
+        logout();
+    }
 });
+
+// Add a separate event listener for the create  game
+document.addEventListener("click", async (event) => {
+    if (event.target && event.target.id === "create-game") {
+        event.preventDefault();
+        await createGame();
+    } else if (event.target && event.target.classList.contains("join-game")) {
+        event.preventDefault();
+        const gameId = event.target.getAttribute("data-game-id");
+        await joinGame(gameId);
+    }
+});
+
+
+//TODO ... change the implementation
+async function createGame() {
+
+    const tablename = document.querySelector('#create-game-form input[tableName="tableName"]').value;
+    const maxPlayers = document.querySelector('#create-game-form input[maxPlayers="maxPlayers"]').value;
+    const minBuyin = document.querySelector('#create-game-form input[minBuyIn="minBuyIn"]').value;
+    const maxBuyIn = document.querySelector('#create-game-form input[maxBuyIn="maxBuyIn"]').value;
+
+    const token = localStorage.getItem("token");
+
+    try{
+      const response = await  fetch("/game/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", 
+        Authorization: `Bearer ${token}` 
+      },
+      body: JSON.stringify(Object.fromEntries(formData)),
+    })
+
+    const responseData = await response.json();
+
+
+    if ( response.status === 200){
+      messageDiv.textContent = "Joined Game"
+    }
+
+
+
+
+
+    }
+
+   
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.error) {
+                showMessage(data.error);
+            } else {
+                window.location.href = `/game/${data.gameId}`;
+            }
+        });
+}
+
+async function joinGame(gameId) {
+    // Join game logic here
+    window.location.href = `/game/join/${gameId}`;
+}
+
+
+
+//SHOW LIST OF GAMES
+async function getGameList() {
+    //TODO CHANGE IMPLEMENTATION
+    const response = await fetch("/game/list");
+    const games = await response.json();
+
+    const gameListElement = document.getElementById("game-list");
+
+    if (games.length === 0) {
+        gameListElement.innerHTML = "<p>No games available. Create one!</p>";
+    } else {
+        const gameItems = games.map((game) => {
+            return `<div>
+                <h3>${game.tableName}</h3>
+                <p>Players: ${game.players.length}/${game.maxPlayers}</p>
+                <p>Buy-In: ${game.minBuyIn} - ${game.maxBuyIn}</p>
+                <button class="join-game" data-game-id="${game._id}">Join</button>
+              </div>`;
+        });
+        gameListElement.innerHTML = gameItems.join("");
+    }
+}
